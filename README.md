be715010714673aadc3b4b64dac08e0d849538e6
David Camelo <NICOLAS.CAMELO28@outlook.com>
2025-05-20 18:15:40 -0500

    Arranged files in folders to include same learning path but now implemented with R language

--- README.md
+++ README.md
@@ -6,8 +6,18 @@ The main course is https://www.udemy.com/course/visualizacion-interactiva-con-py
 
 In general this course is a good introduction to implement these tools, step by step, since create basic visuals until create interactive dashboards.
 
+The next images are just for the Python folder
+
 ![First Steps](image-1.png)
 
 ![Scatter](image-2.png)
 
 ![Final Dash Course](image.png)
+
+
+# Now with R
+
+And the course to learn this kind of visualization for R is:
+https://www.udemy.com/course/el-arte-de-programar-en-r-anade-valor-a-tu-cv/?couponCode=ST20MT190425G1 
+
+![alt text](image-3.png)
---
+++ interactive_dashboard_with_R/first_interactive_dashboad_with_r.R
@@ -0,0 +1,62 @@
+# First time implementing shinny with plotly to create interactive dashboards
+
+#Required Libraries
+library(shiny)
+library(ggplot2)
+library(ggiraph)
+library(plotly)
+library(DT)
+
+# Shiny apps are composed by main components, like de UI and the server
+
+# Creating UI 
+ui <- fluidPage(
+  titlePanel("Interactive Dashboard"),                                                # Title for the page
+  sidebarLayout(                                                                     # Create a layout for a side page panel
+    sidebarPanel( 
+      selectInput("var", "Selecting Variable:",                                      # Create a slicer
+                  choices = c("mpg", "hp", "wt"),                                    # Include options
+                  selected = "mpg")                                                  # Default value 
+    ),
+    mainPanel(                                                                       # Create a main paned for the dashboard
+      tabsetPanel(
+        tabPanel("Interactive chart with ggirapg", girafeOutput("plot_ggiraph")),    # Add a plot created with ggiraph and mark the id
+        tabPanel("Interactive chart with plotly", plotlyOutput("plot_plotly")),      # Add a plot created with plotly and mark the id
+        tabPanel("Interactive Table", DTOutput("table"))                             # Add a table created with DT and mark the id
+      )
+    )
+  )
+)
+
+print(ui)
+
+#Creatin server section
+
+server <- function(input, output) {
+
+  # interactive chart with ggiraph
+  output$plot_ggiraph <- renderGirafe({
+    p <- ggplot(mtcars, aes_string(x = 'wt', y = input$var, color = "factor(cyl)")) +
+      geom_point_interactive(aes(tooltip = paste("Value:", input$var))) +
+      labs(title =  "Interactive chart with ggiraph")
+    girafe(ggobj =  p)
+  })
+
+  # Interactive chart with plotly
+  output$plot_plotly <- renderPlotly({
+    p2 <- ggplot(mtcars, aes_string(x = "wt", y= input$var, color = "factor(cyl)")) + 
+      geom_point(size = 4) +
+      labs(title = "Plotly chart")
+    ggplotly(p2)
+  })
+
+  #interavtive table
+  output$table <- renderDT({
+    datatable(mtcars, options = list(pageLength = 5))
+  })
+}
+
+print(server)
+
+# Render the final dashboard
+shinyApp(ui, server)
